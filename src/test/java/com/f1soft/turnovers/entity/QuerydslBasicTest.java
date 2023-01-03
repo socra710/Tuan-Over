e9f8a3ff9f4e10324702960a3f971288cdaea78b
chuseok <90127lucy@gmail.com>
2023-01-03 15:38:04 +0900

    paging 예시 추가

--- src/test/java/com/f1soft/turnovers/entity/QuerydslBasicTest.java
+++ src/test/java/com/f1soft/turnovers/entity/QuerydslBasicTest.java
@@ -135,5 +135,30 @@ public class QuerydslBasicTest {
         assertThat(memberNull.getUsername()).isNull();
     }
 
-    //commit test
+    @Test
+    public void paging1() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)//index 1~3
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)//index 1~3
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
 }
