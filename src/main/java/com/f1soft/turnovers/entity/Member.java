13d40ac6781ea29ea1207406031433fdc711beca
chuseok <90127lucy@gmail.com>
2023-01-04 14:55:49 +0900

    member 복구

--- src/main/java/com/f1soft/turnovers/entity/Member.java
+++ src/main/java/com/f1soft/turnovers/entity/Member.java
@@ -1,21 +1,46 @@
 package com.f1soft.turnovers.entity;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 
-@Getter
 @Entity
-@Table(name = "members")
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    //manytoone은 fetch lazy 해줘야함
+    @JoinColumn(name = "team_id")
+    private Team team;
 
-    @Column(length = 30, nullable = false, unique = true)
-    private String email;
+    public Member(String username) {
+        this(username, 0, null);
+    }
 
-    @Column(length = 20, nullable = false)
-    private String name;
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
--- src/main/java/com/f1soft/turnovers/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.f1soft.turnovers.repository;
-
-import com.f1soft.turnovers.entity.Link;
-import com.f1soft.turnovers.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Member findByEmail(String email);
-}
